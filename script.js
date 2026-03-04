04324cda4a32dcf6eaefc9fcaa550ff2d6637b03
MarcoZ23 <zollomarco23@gmail.com>
2026-03-04 21:09:05 +0100

    complete task four

--- script.js
+++ script.js
@@ -25,7 +25,10 @@ teachers.pop();
   
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+teachers.shift(); 
+  console.log(firstTeacher, teachers);
+  
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
