fdfb6fd466df1c89b146885f43e10ae025b9d477
MarcoZ23 <zollomarco23@gmail.com>
2026-03-04 21:06:42 +0100

    complete task three

--- script.js
+++ script.js
@@ -15,11 +15,14 @@ const fourthTeacher = teachers[3];
   console.log(fourthTeacher);
   
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers[4] = "Patrick"
+teachers[4] = "Patrick";
   console.log(teachers[4]);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers[6];
+teachers.pop();
+  console.log(lastTeacher, teachers);
+  
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
